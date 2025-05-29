6a358baa254019229c3f01112017ae73f559dfd4
Robert Keresnyei <rk@nemron.com>
2025-05-29 07:31:28 +0200

    Add specs

--- spec/bootstrap_concerns/components_helper_spec.rb
+++ spec/bootstrap_concerns/components_helper_spec.rb
@@ -1,12 +1,71 @@
+require "action_view"
 require "bootstrap_concerns/components_helper"
+require "bootstrap_concerns/option"
 
 RSpec.describe BootstrapConcerns::ComponentsHelper do
-  describe "#flash_alert_color" do
-    subject(:flash_alert_color) { helper_class.new.flash_alert_color(key) }
+  let(:helper) do
+    Class
+      .new do
+        include BootstrapConcerns::ComponentsHelper
+        include ActionView::Helpers::UrlHelper
+      end
+      .new
+  end
 
-    let(:helper_class) do
-      Class.new { include BootstrapConcerns::ComponentsHelper }
+  describe "#bs_link_to" do
+    before do
+      allow(helper).to receive(:link_to)
+
+      helper.bs_link_to(name, options, html_options, &block)
     end
+
+    context "with block" do
+      let(:name) { "#" }
+      let(:html_options) { nil }
+      let(:block) { proc { "Click me" } }
+
+      context "with options" do
+        let(:options) { {size: "sm"} }
+
+        it "calls link_to with the Bootstrap options" do
+          expect(helper).to have_received(:link_to).with(name, {class: "btn btn-primary btn-sm"}, nil)
+        end
+      end
+
+      context "without options" do
+        let(:options) { nil }
+
+        it "calls link_to with the Bootstrap options" do
+          expect(helper).to have_received(:link_to).with(name, {class: "btn btn-primary"}, nil)
+        end
+      end
+    end
+
+    context "without block" do
+      let(:name) { "Click me" }
+      let(:options) { "#" }
+      let(:block) { nil }
+
+      context "with html_options" do
+        let(:html_options) { {size: "sm"} }
+
+        it "calls link_to with the Bootstrap html_options" do
+          expect(helper).to have_received(:link_to).with(name, options, {class: "btn btn-primary btn-sm"})
+        end
+      end
+
+      context "without html_options" do
+        let(:html_options) { nil }
+
+        it "calls link_to with the Bootstrap html_options" do
+          expect(helper).to have_received(:link_to).with(name, options, {class: "btn btn-primary"})
+        end
+      end
+    end
+  end
+
+  describe "#flash_alert_color" do
+    subject(:flash_alert_color) { helper.flash_alert_color(key) }
 
     context "with notice key" do
       let(:key) { "notice" }
